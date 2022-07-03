43dae05a156aee388e5b1d5719dfded0e8660478
RhaskiaGameDev <94507929+RhaskiaGameDev@users.noreply.github.com>
2022-07-04 09:31:53 +1200

    namespaces. cleaner code? sorta.

--- Assets/Scripts/Creature/Movement.cs
+++ Assets/Scripts/Creature/Movement.cs
@@ -4,214 +4,222 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class MoveInput
+namespace Creature
 {
-    public Vector2 movement;
-    public bool crouch;
-    public bool trot, run;
-    public bool jump, glide, fly;
-    public bool flyDown, flyUp;
-
-    public MoveInput(Vector2 m, bool _crouch, bool _trot, bool _run,
-        bool _jump, bool _glide, bool _fly, bool _fDown, bool _fUp)
+    public class MoveInput
     {
-        movement = m; crouch = _crouch; trot = _trot; run = _run; jump = _jump; glide = _glide; fly = _fly; flyDown = _fDown; flyUp = _fUp;
-    }
-}
+        public Vector2 movement;
+        public bool crouch;
+        public bool trot, run;
+        public bool jump, glide, fly;
+        public bool flyDown, flyUp;
 
-public class Movement : MonoBehaviour
-{
-    [Header("Relations")]
-    PlayerManager playerM;
-    public Rigidbody rb;
-    public MoveInput moveInput;
-    public Creature creature;
-
-    [Header("Variables")]
-    public float stamina;
-    public float maxStamina;
-    public float jumpForce;
-
-    [Header("GroundCheck")]
-    public float groundcheckRadius = 0.1f;
-    public LayerMask groundLayer;
-    public Transform groundcheck;
-    public bool onGround;
-
-
-
-    bool crouching, gliding, flying;
-
-    [Header("Gliding")]
-    public float maxGlideSpeed;
-    public float glideEfficieny;
-    public Vector3 glideDir;
-
-    [Header("Swimming")]
-    public LayerMask waterLayer;
-    public Transform watercheck;
-    public float buoyancy;
-    public bool inWater;
-
-    PhotonView pv;
-
-    private void Awake()
-    {
-        pv = GetComponentInParent<PhotonView>();
-        if (!pv.IsMine) return;
-
-        GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
-        playerM = FindObjectOfType<PlayerManager>();
-        creature = playerM.creature;
+        public MoveInput(Vector2 m, bool _crouch, bool _trot, bool _run,
+            bool _jump, bool _glide, bool _fly, bool _fDown, bool _fUp)
+        {
+            movement = m; crouch = _crouch; trot = _trot; run = _run; jump = _jump; glide = _glide; fly = _fly; flyDown = _fDown; flyUp = _fUp;
+        }
     }
 
-    private void OnDestroy()
+    public class Movement : MonoBehaviour
     {
-        GameStateManager.Instance.OnGameStateChanged -= OnGameStateChanged;
-    }
+        [Header("Relations")]
+        PlayerManager playerM;
+        public Rigidbody rb;
+        public MoveInput moveInput;
+        public Creature creature;
 
-    private void FixedUpdate()
-    {
-        if (moveInput == null || !playerM.pv.IsMine)
-            return;
+        [Header("Variables")]
+        public float stamina;
+        public float maxStamina;
+        public float jumpForce;
 
-        Vector3 speed = new Vector3(moveInput.movement.normalized.x, 0, moveInput.movement.normalized.y);
-        float speedMult = GetSpeed();
+        [Header("GroundCheck")]
+        public float groundcheckRadius = 0.1f;
+        public LayerMask groundLayer;
+        public Transform groundcheck;
+        public bool onGround;
 
-        if (Mathf.Abs(speed.x) + Mathf.Abs(speed.z) > 0.75f) glideDir = speed;
+        bool crouching, gliding, flying;
 
-        //Applying Input If On Ground
-        Vector3 s = transform.right * moveInput.movement.normalized.x * speedMult * ((playerM.growth.currentPercent / 2f) + 50f) / 100f;
-        Vector3 f = transform.forward * moveInput.movement.normalized.y * speedMult * ((playerM.growth.currentPercent / 2f) + 50f) / 100f;
+        [Header("Gliding")]
+        public float maxGlideSpeed;
+        public float glideEfficieny;
+        public Vector3 glideDir;
 
-        if (onGround || flying || inWater) rb.velocity = new Vector3(s.x + f.x, rb.velocity.y, s.z + f.z);
+        [Header("Swimming")]
+        public LayerMask waterLayer;
+        public Transform watercheck;
+        public float buoyancy;
+        public bool inWater;
+        float lastInWater;
 
-        //Stamina
-        stamina += 2;
-        stamina = Mathf.Clamp(stamina, 0, maxStamina);
+        PhotonView pv;
 
-    }
-    void Update()
-    {
-        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, RoomManager.Instance.rotation, transform.rotation.z));
-
-        if (moveInput == null || !playerM.pv.IsMine)
-            return;
-
-        //Groundcheck
-        onGround = Physics.Raycast(groundcheck.position, transform.TransformDirection(Vector3.down), 0.5f, groundLayer);
-
-        //Swimming
-        if (inWater)
+        private void Awake()
         {
-            flying = false;
-            gliding = false;
-            crouching = false;
+            pv = GetComponentInParent<PhotonView>();
+            if (!pv.IsMine) return;
 
-            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.one * buoyancy, 0.99f);
-
-            if (moveInput.flyUp) rb.AddForce(new Vector3(0, 0.1f, 0), ForceMode.Impulse);
-            if (moveInput.flyDown) rb.AddForce(new Vector3(0, -0.1f, 0), ForceMode.Impulse);
+            GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
+            playerM = FindObjectOfType<PlayerManager>();
+            creature = playerM.creature;
         }
 
-        //Flying
-        if (flying)
+        private void OnDestroy()
         {
-            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.one * -0.01f, 0.99f);
-
-            if (moveInput.flyUp) rb.AddForce(new Vector3(0, 0.1f, 0), ForceMode.Impulse);
-            if (moveInput.flyDown) rb.AddForce(new Vector3(0, -0.1f, 0), ForceMode.Impulse);
-        }
-        rb.useGravity = !flying;
-
-        //Gliding
-        if (gliding)
-        {
-            float mult = 1 + (glideEfficieny * Time.deltaTime);
-            glideDir = new Vector3(Mathf.Clamp(glideDir.x * mult, -maxGlideSpeed, maxGlideSpeed), -0.1f, Mathf.Clamp(glideDir.z * mult, -maxGlideSpeed, maxGlideSpeed));
-            rb.velocity = glideDir;
+            GameStateManager.Instance.OnGameStateChanged -= OnGameStateChanged;
         }
 
-        //Managing stuff
-        if (onGround)
+        private void FixedUpdate()
         {
-            gliding = false; flying = false;
+            if (moveInput == null || !playerM.pv.IsMine)
+                return;
 
-            if (moveInput.jump) rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-            if (moveInput.crouch) crouching = !crouching;
+            Vector3 speed = new Vector3(moveInput.movement.normalized.x, 0, moveInput.movement.normalized.y);
+            float speedMult = GetSpeed();
+
+            if (Mathf.Abs(speed.x) + Mathf.Abs(speed.z) > 0.75f) glideDir = speed;
+
+            //Applying Input If On Ground
+            Vector3 s = transform.right * moveInput.movement.normalized.x * speedMult * ((playerM.growth.currentPercent / 2f) + 50f) / 100f;
+            Vector3 f = transform.forward * moveInput.movement.normalized.y * speedMult * ((playerM.growth.currentPercent / 2f) + 50f) / 100f;
+
+            if (onGround || flying || inWater) rb.velocity = new Vector3(s.x + f.x, rb.velocity.y, s.z + f.z);
+
+            //Stamina
+            stamina += 2;
+            stamina = Mathf.Clamp(stamina, 0, maxStamina);
+
         }
-        else
+        void Update()
         {
-            crouching = false;
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, RoomManager.Instance.rotation, transform.rotation.z));
 
-            if (moveInput.glide)
+            if (moveInput == null || !playerM.pv.IsMine)
+                return;
+
+            //Groundcheck
+            onGround = Physics.Raycast(groundcheck.position, transform.TransformDirection(Vector3.down), 0.5f, groundLayer);
+
+            //Swimming
+            if (inWater)
             {
-                gliding = !gliding;
                 flying = false;
-            }
-
-            if (moveInput.fly)
-            {
-                flying = !flying;
                 gliding = false;
+                crouching = false;
+
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.one * buoyancy, 0.99f);
+
+                if (moveInput.flyUp) rb.AddForce(new Vector3(0, 0.1f, 0), ForceMode.Impulse);
+                if (moveInput.flyDown) rb.AddForce(new Vector3(0, -0.1f, 0), ForceMode.Impulse);
+
+                lastInWater = Time.time;
+            }
+            else if (Time.time - lastInWater < 2)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.one * (1 - (Time.time - lastInWater / 2)) * buoyancy, 0.99f);
+            }
+
+            //Flying
+            if (flying)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.one * -0.01f, 0.99f);
+
+                if (moveInput.flyUp) rb.AddForce(new Vector3(0, 0.1f, 0), ForceMode.Impulse);
+                if (moveInput.flyDown) rb.AddForce(new Vector3(0, -0.1f, 0), ForceMode.Impulse);
+            }
+            rb.useGravity = !flying;
+
+            //Gliding
+            if (gliding)
+            {
+                float mult = 1 + (glideEfficieny * Time.deltaTime);
+                glideDir = new Vector3(Mathf.Clamp(glideDir.x * mult, -maxGlideSpeed, maxGlideSpeed), -0.1f, Mathf.Clamp(glideDir.z * mult, -maxGlideSpeed, maxGlideSpeed));
+                rb.velocity = glideDir;
+            }
+
+            //Managing stuff
+            if (onGround)
+            {
+                gliding = false; flying = false;
+
+                if (moveInput.jump) rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+                if (moveInput.crouch) crouching = !crouching;
+            }
+            else
+            {
+                crouching = false;
+
+                if (moveInput.glide)
+                {
+                    gliding = !gliding;
+                    flying = false;
+                }
+
+                if (moveInput.fly)
+                {
+                    flying = !flying;
+                    gliding = false;
+                }
+            }
+
+            //Crouching
+            if (onGround && moveInput.crouch) crouching = !crouching;
+        }
+
+        public float GetSpeed()
+        {
+            //Trotting
+            if (moveInput.trot)
+            {
+                stamina -= creature.runSpeed.staminaUse;
+                if (stamina > creature.runSpeed.minStamina) return creature.runSpeed.speed;
+                crouching = false;
+            }
+
+            //Running
+            if (moveInput.run)
+            {
+                stamina -= creature.runSpeed.staminaUse;
+                if (stamina > creature.runSpeed.minStamina) return creature.runSpeed.speed;
+                crouching = false;
+            }
+
+            //Crouching
+            if (crouching) return creature.sneakSpeed.speed;
+
+            stamina -= creature.walkSpeed.staminaUse;
+            return creature.walkSpeed.speed;
+        }
+
+        private void OnGameStateChanged(GameState newGameState)
+        {
+            enabled = newGameState == GameState.GamePlay;
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Water")
+            {
+                inWater = true;
             }
         }
 
-        //Crouching
-        if (onGround && moveInput.crouch) crouching = !crouching;
-    }
-
-    public float GetSpeed()
-    {
-        //Trotting
-        if (moveInput.trot)
+        void OnTriggerStay(Collider other)
         {
-            stamina -= creature.runSpeed.staminaUse;
-            if (stamina > creature.runSpeed.minStamina) return creature.runSpeed.speed;
-            crouching = false;
+            if (other.gameObject.tag == "Water")
+            {
+                inWater = true;
+            }
         }
 
-        //Running
-        if (moveInput.run)
+        void OnTriggerExit(Collider other)
         {
-            stamina -= creature.runSpeed.staminaUse;
-            if (stamina > creature.runSpeed.minStamina) return creature.runSpeed.speed;
-            crouching = false;
-        }
-
-        //Crouching
-        if (crouching) return creature.sneakSpeed.speed;
-
-        stamina -= creature.walkSpeed.staminaUse;
-        return creature.walkSpeed.speed;
-    }
-
-    private void OnGameStateChanged(GameState newGameState)
-    {
-        enabled = newGameState == GameState.GamePlay;
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Water")
-        {
-            inWater = true;
-        }
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Water")
-        {
-            inWater = true;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Water")
-        {
-            inWater = false;
+            if (other.gameObject.tag == "Water")
+            {
+                inWater = false;
+            }
         }
     }
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,35 +1,38 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class PlayerInput : MonoBehaviour
+namespace Player
 {
-    [Header("Keys")]
-    public KeyCode trot;
-    public KeyCode run, jump, crouch, glide, fly;
-
-    public Movement move;
-    PhotonView pv;
-
-    void Start()
+    public class PlayerInput : MonoBehaviour
     {
-        pv = GetComponent<PhotonView>();
+        [Header("Keys")]
+        public KeyCode trot;
+        public KeyCode run, jump, crouch, glide, fly;
+
+        public Creature.Movement move;
+        PhotonView pv;
+
+        void Start()
+        {
+            pv = GetComponent<PhotonView>();
+        }
+
+        void Update()
+        {
+            if (!pv.IsMine) return;
+
+            //Input
+            Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            move.moveInput = GetInput();
+        }
+
+        Creature.MoveInput GetInput()
+        {
+            return new Creature.MoveInput(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")),
+                Input.GetKeyDown(crouch), Input.GetKey(trot), Input.GetKey(run),
+                Input.GetKeyDown(jump), Input.GetKeyDown(glide), Input.GetKeyDown(fly),
+                Input.GetKey(crouch), Input.GetKey(jump));
+        }
+
     }
-
-    void Update()
-    {
-        if (!pv.IsMine) return;
-
-        //Input
-        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        move.moveInput = GetInput();
-    }
-
-    MoveInput GetInput()
-    {
-        return new MoveInput(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")),
-            Input.GetKeyDown(crouch), Input.GetKey(trot), Input.GetKey(run),
-            Input.GetKeyDown(jump), Input.GetKeyDown(glide), Input.GetKeyDown(fly),
-            Input.GetKey(crouch), Input.GetKey(jump));
-    }
-
 }
