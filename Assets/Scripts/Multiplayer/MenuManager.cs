f78f018e2c63cdb9fb7e92770f367e03deaf2f6e
RhaskiaGameDev <94507929+RhaskiaGameDev@users.noreply.github.com>
2022-07-12 09:08:00 +1200

    uh huh

--- Assets/Scripts/Multiplayer/MenuManager.cs
+++ Assets/Scripts/Multiplayer/MenuManager.cs
@@ -7,47 +7,18 @@ public class MenuManager : MonoBehaviour
     public static MenuManager Instance;
 
     [SerializeField] Menu[] menus;
-    string lastMenu = "Loading";
 
-    [SerializeField] float loadingTime;
-    float lastLoad;
-    string menuToLoad;
 
     public void Awake()
     {
         Instance = this;
-        lastMenu = "Loading";
     }
 
     public void OpenMenu(string menu)
     {
-        if (lastMenu == "Loading")
-        {
-            StartCoroutine("LoadMenuDelay");
-            menuToLoad = menu;
-        }
-        else
-        {
-            foreach (var _menu in menus)
-            {
-                if (menu == _menu.menuName) _menu.Open();
-                else CloseMenu(_menu);
-            }
-        }
-
-
-        lastMenu = menu;
-        if (lastMenu == "Loading") lastLoad = Time.timeSinceLevelLoad;
-    }
-
-    IEnumerator LoadMenuDelay()
-    {
-        while (Time.timeSinceLevelLoad - lastLoad < loadingTime)
-            yield return null;
-
         foreach (var _menu in menus)
         {
-            if (menuToLoad == _menu.menuName) _menu.Open();
+            if (menu == _menu.menuName) _menu.Open();
             else CloseMenu(_menu);
         }
     }
