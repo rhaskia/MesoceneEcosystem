473ca16d33919522dee3db5504098e73dd7836ea
RhaskiaGameDev <94507929+RhaskiaGameDev@users.noreply.github.com>
2022-07-15 11:01:34 +1200

    uh huh

---
+++ Assets/CreatureSelector.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatureSelector : MonoBehaviour
+{
+    Creature.Creature[] creatures;
+    public Image[] images;
+    public int current;
+    public int offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creatures = RoomManager.Instance.creatures;
+
+        UpdateSelector();
+    }
+
+    void Update()
+    {
+        UpdateSelector();
+    }
+
+    // Update is called once per frame
+    void UpdateSelector()
+    {
+        for (int i = 0; i < images.Length; i++)
+        {
+            var sprite = creatures[i - current].idle.side[0];
+            images[i].sprite = sprite;
+            images[i].transform.localScale = new Vector3(images[i].rectTransform.rect.width, (images[i].rectTransform.rect.width / sprite.rect.width) * sprite.rect.height);
+        }
+    }
+}
